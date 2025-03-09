96fd82b3fa2ee613e8628cea6097f893fc7aafa9
unknown <saimeshchathurak@gmail.com>
2025-03-09 21:54:40 +0530

    initial test

---
+++ aaa.c
@@ -0,0 +1,12 @@
+#include <stdio.h> int main(){
+//variable declaration char name[20]; int a, b; flot c, f;
+//variable initialization char val[] = "Welcome"; a= 10;
+//Defining Constants const float x = 3.73
+//Inteaction with the user printf("Enter your name: ); scanf("%s", &name); printf(val);
+printf("\nYour name is %d.", name); c= 90.0*3.0
+printf("\nValue of c: %f", c); x= 49;
+printf("\nValue of X: %f", x);
+//using the escape sequances printf("Hello\tWorld\n\n");
+printf("You\nare\nlearning\n\'c\' language\n\"Do you know C language\"");
+return 0;
+}
---
+++ first.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main (){
+	printf("Hello, Imesh!\n");
+	return 0;
+}
